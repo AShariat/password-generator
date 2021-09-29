c06348bead6a2ed67eb0a7c669aa7797ddc46f26
Amir Shariat <amirshariat1987@gmail.com>
2021-09-29 14:20:46 -0600

    added function to check the password's length

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,32 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var generatePassword = function() {
+  var passwordLength = window.prompt("How many characters would you like your password to contain?");
+  // passwordLength = parseInt(passwordLength);
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    console.log(passwordLength);
+    password = "a password length";
+    return password;
+  } else if (passwordLength < 8) {
+    console.log(passwordLength);
+    window.alert("Password length must be at least 8 characters!");
+    generatePassword();
+  } else if (passwordLength > 128) {
+    console.log(passwordLength);
+    window.alert("Password length must be no more than 128 characters!");
+    generatePassword();
+  } else {
+    window.alert("Password length must be provided as a number!");
+    generatePassword();
+  }
+};
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  // var password = generatePassword();
+  generatePassword();
+  console.log(password);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;

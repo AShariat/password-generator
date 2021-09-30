7904928ce76b35b132f0c508577d24d47f12668d
Amir Shariat <amirshariat1987@gmail.com>
2021-09-29 23:16:15 -0600

    added random character function and for loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,22 @@
 var generateBtn = document.querySelector("#generate");
 
+var randomNumber = function() {
+  var value = Math.floor(Math.random() * 94) + 33;
+  // console.log(value);
+  return value;
+};
+
+var randomCharacters = function() {
+  var value = String.fromCharCode(randomNumber());
+  // console.log(value);
+  return value;
+};
+
 var passwordLength = function() {
   var value = window.prompt("How many characters would you like your password to contain?");
   value = parseInt(value);
   if (value >= 8 && value <= 128) {
-    console.log("The password's length is " + value);
+    // console.log("The password's length is " + value);
   } else if (value < 8) {
     window.alert("Password length must be at least 8 characters!");
     return passwordLength();
@@ -41,36 +53,50 @@ var uppercaseCharacters = function() {
   return value;
 };
 
-var generatePassword = function() {
-  var passLength = passwordLength();
-  console.log(passLength);
+var passwordType = function() {
   if (specialCharacters()) {
-    console.log("SC");
+    // console.log("SC");
     var special = true;
   }
   if (numericCharacters()) {
-    console.log("NC");
+    // console.log("NC");
     var numeric = true;
   }
   if (lowercaseCharacters()) {
-    console.log("LC");
+    // console.log("LC");
     var lowercase = true;
   }
   if (uppercaseCharacters()) {
-    console.log("UC");
+    // console.log("UC");
     var uppercase = true;
   }
   if (special || numeric || lowercase || uppercase) {
-    console.log("Password");
-    return "Password";
+    var value = randomCharacters();
+    // console.log(value);
   } else {
     console.log("Error");
     window.alert("Must select at least one character type.");
-  }  
+  }
+  return value;
+};
+
+var generatePassword = function() {
+  var passLength = passwordLength();
+  passLength = parseInt(passLength);
+  // console.log(passLength);
+  passwordType();
+  var value = "";
+  for (var i = 0; i < passLength; i++) {
+    // console.log(randomCharacters());
+    value += randomCharacters();
+  }
+  console.log(value);
+  return value;
 };
 
 // Write password to the #password input
 function writePassword() {
+  // generatePassword();
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
